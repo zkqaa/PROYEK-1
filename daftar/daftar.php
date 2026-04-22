1ee06c49e34d67c528c83d49d5ace993de5327ba
sitiaidafitriyah12 <sitiaidafitriyah@gmail.com>
2026-04-22 08:52:35 +0700

    Update daftar.php

--- daftar/daftar.php
+++ daftar/daftar.php
@@ -5,6 +5,7 @@ $nohp = $_POST['nohp'];
 $password = $_POST['password'];
 $konfirmasi = $_POST['konfirmasi'];
 
+//Cek password
 if ($password !== $konfirmasi) {
     echo "<script>
         alert('Password tidak sama!');
@@ -13,8 +14,10 @@ if ($password !== $konfirmasi) {
     exit();
 }
 
+//Enkripsi password
 $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
+//Cek no.hp apakah sudah ada atau belum
 $cek = mysqli_query($conn, "SELECT * FROM users WHERE nohp='$nohp'");
 
 if (mysqli_num_rows($cek) > 0) {
